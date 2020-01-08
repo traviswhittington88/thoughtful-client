a933a4331b4dd5c4333703f06ab689d87b3f9c3d
traviswhittington88 <traviswhittington@ymail.com>
2020-01-07 20:50:28 -0600

    basic styling of login page

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './LoginPage.css'
 import LoginForm from '../../components/LoginForm/LoginForm'
 import Nav from '../../components/Nav/Nav'
 import Footer from '../../components/Footer/Footer'
@@ -21,12 +22,16 @@ export default class LoginPage extends Component {
       return (
         <>
           <Nav />
-          <section className='LoginPage'>
-            <h2>Login!</h2>
-            <LoginForm
-              onLoginSuccess={this.handleLoginSuccess}
-            />
-          </section>
+          <main role="main">
+            <section className='LoginPage'>
+              <header role="banner">
+                <h2>Login</h2>
+              </header>
+              <LoginForm
+                onLoginSuccess={this.handleLoginSuccess}
+              />
+            </section>
+          </main>
           <Footer />
         </>
       )
