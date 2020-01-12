cf350237b319900a30759dcc22e3ab8167172dfe
traviswhittington88 <traviswhittington@ymail.com>
2020-01-11 21:48:36 -0600

    testing styles

---
+++ src/components/AddEntryForm/AddEntryForm.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './AddEntryForm.css'
+
+export default class AddEntryForm extends Component {
+  render() {
+    return (
+      <form className="add-entry-form">
+        <div className="form-content">
+          <label htmlFor="entry-title" className="label entry-title">Entry Title</label>
+          <input type="text" name="entry-title" id="entry-title" required />
+        </div>
+        <div className="form-content">
+          <label htmlFor="entry-text" className="label entry-text">A place for your thoughts</label>
+          <textarea name="entry-text" rows="30"></textarea>
+        </div>
+        <div className="form-content">
+          <label htmlFor="journal-select" className="label journal-select">Select Journal</label>
+            <select>No Journals</select>
+        </div>
+        <button type="submit" className="add-entry-button">Submit</button>
+      </form>
+    )
+  }
+}
--- src/routes/AddEntryPage/AddEntryPage.js
+++ src/routes/AddEntryPage/AddEntryPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './AddEntryPage.css'
+import AddEntryForm from '../../components/AddEntryForm/AddEntryForm'
 import Nav from '../../components/Nav/Nav'
 import Footer from '../../components/Footer/Footer'
 
@@ -9,25 +10,11 @@ export default class AddEntryPage extends Component {
     <>
       <Nav />
       <main role="main">
+        <section className="add-entry-page">
         <header role="banner" className="add-entry-header">
           <h1 className="add-entry-heading">New Entry</h1>
         </header>
-        <section className="form-section">
-          <form id="add-entry-form">
-            <div className="form-content">
-              <label htmlFor="entry-title" className="entry-title">Entry Title</label>
-              <input type="text" name="entry-title" id="entry-title" required />
-            </div>
-            <div className="form-content">
-              <label htmlFor="entry-text">A place for your thoughts</label>
-              <textarea name="entry-text" rows="30"></textarea>
-            </div>
-            <div className="form-content">
-              <label htmlFor="journal-selector">Select Journal</label>
-                <select>No Journals</select>
-            </div>
-            <button type="submit" className="add-entry-button">Submit</button>
-          </form>
+          <AddEntryForm />
         </section>
       </main>
       <Footer />
