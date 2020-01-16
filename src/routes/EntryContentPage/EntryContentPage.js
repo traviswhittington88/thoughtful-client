97a42dc028450de76cc40ce0e71ad363308cfef2
traviswhittington88 <traviswhittington@ymail.com>
2020-01-16 13:34:44 -0600

    created EntryContentPage route component with stylesheet

--- src/routes/EntryContentPage/EntryContentPage.js
+++ src/routes/EntryContentPage/EntryContentPage.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
+import Nav from '../../components/Nav/Nav'
+import Footer from '../../components/Footer/Footer'
+import './EntryContentPage.css'
 
 export default class EntryContentPage extends Component {
   render() {
-    return <></>
+    return (
+      <>
+        <Nav />
+          <main role="main" className="entryContentPage">
+
+          </main>
+        <Footer />
+      </>
+    )
   }
 }
